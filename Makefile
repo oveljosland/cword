22b6021f1872e87898f74165404b02fcb9136128
Ove Ljosland <oveljosland@icloud.com>
2024-12-25 13:45:46 +0100

    initial

---
+++ Makefile
@@ -0,0 +1,2 @@
+cword: cword.c
+	$(CC) cword.c -o cword -Wall -Wextra -pedantic -std=c99
---
+++ cword.c
@@ -0,0 +1,37 @@
+#include <ctype.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <termios.h>
+#include <unistd.h>
+
+struct termios original_termios; // original terminal io settings
+
+void disable_raw_mode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &original_termios);
+}
+
+void enable_raw_mode() {
+    tcgetattr(STDIN_FILENO, &original_termios);
+    atexit(disable_raw_mode); // disable raw mode on exit
+
+    struct termios raw = original_termios;
+    raw.c_lflag &= ~(ECHO | ICANON);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+
+int main() {
+    enable_raw_mode();
+
+    char c;
+    while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q') {
+        if (iscntrl(c)) { // check if c is a control char
+            printf("%d\n", c); // format byte c as decimal
+        } else {
+            printf("%d ('%c')\n", c, c); // format byte c as character
+        }
+    }
+
+    return 0;
+}
